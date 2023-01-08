777a90fbc3e85942eb1aa7acc136f722d90edf4c
aizzaf <swashbuckleizza@gmail.com>
2023-01-08 10:42:12 +0000

    first

---
+++ install-openfoam.sh
@@ -0,0 +1,4 @@
+sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key > /etc/apt/trusted.gpg.d/openfoam.asc"
+sudo add-apt-repository http://dl.openfoam.org/ubuntu
+sudo apt-get update
+sudo apt-get -y install openfoam10
